744a4fd9aa9a5b49806ae8ead9d02f32a3056ed9
siangyeh8818 <siangyeh@gmail.com>
2018-11-09 17:51:55 +0800

    fix some error for docker-entrypoint.sh

--- image-Dockerfile/docker-entrypoint.sh
+++ image-Dockerfile/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [${MY_POD_NAME}='consul-0' ]
+if [${MY_POD_NAME}='consul-cluster-0' ]
 then
+  echo "./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui"
   ./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui
 else
-  ./consul agent -server -join consul-0 -ui  -disable-host-node-id -client 0.0.0.0"
+  echo "./consul agent -server -join consul-cluster-0 -ui  -disable-host-node-id -client 0.0.0.0"
+  ./consul agent -server -join consul-cluster-0 -ui  -disable-host-node-id -client 0.0.0.0
 fi 
