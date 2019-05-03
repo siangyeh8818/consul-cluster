63de2ed262e2e934bfbe2a4433e56bb271d4c9df
siangyeh8818 <siangyeh8818@gmail.com>
2019-05-03 18:39:01 +0800

    fix : 修正原本的image無法在k8s上cluster的問題
    image沒有build 到dockerhub上
    另外也修正原本docker-entrypoint.sh ,並無法讓cluster的功能發揮

--- image-Dockerfile/docker-entrypoint.sh
+++ image-Dockerfile/docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [${MY_POD_NAME}='consul-cluster-0' ]
+mkdir -p /opt/consul/data
+
+if [ ${MY_POD_NAME} = 'consul-cluster-0' ];
 then
-  echo "./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui"
-  ./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui
+  echo $MY_POD_NAME
+  echo "./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui -data-dir=/opt/consul/data"
+  ./consul agent -server -bootstrap-expect 1   -disable-host-node-id -client 0.0.0.0 -ui -data-dir=/opt/consul/data
 else
-  echo "./consul agent -server -join consul-cluster-0 -ui  -disable-host-node-id -client 0.0.0.0"
-  ./consul agent -server -join consul-cluster-0 -ui  -disable-host-node-id -client 0.0.0.0
+  echo $MY_POD_NAME
+  echo "./consul agent -server -join consul-cluster-0.consul-discovery.default.svc.cluster.local:8301 -ui  -disable-host-node-id -client 0.0.0.0 -data-dir=/opt/consul/data"
+  ./consul agent -server -join consul-cluster-0.consul-discovery.default.svc.cluster.local:8301 -ui  -disable-host-node-id -client 0.0.0.0 -data-dir=/opt/consul/data
 fi 
